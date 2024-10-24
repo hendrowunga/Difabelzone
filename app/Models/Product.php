1bcce7919d9bf0ca55500444b888f95f25a9f65e
Hendro <hendrowunga073@gmail.com>
2024-10-24 21:36:37 +0700

    add product terbaru

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 // Ini adalah Model Product
+//Jadi kita harus melakukan php make:artisan model Product
 class Product extends Model
 {
     use HasFactory;
